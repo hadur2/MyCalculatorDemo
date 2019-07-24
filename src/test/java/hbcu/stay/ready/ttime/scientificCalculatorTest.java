84624855eb82232ae7e966dd24fef38a9867374c
hadur2 <hadur@morgan.edu>
2019-07-24 11:30:18 -0400

    done

---
+++ src/test/java/hbcu/stay/ready/ttime/scientificCalculatorTest.java
@@ -0,0 +1,20 @@
+package hbcu.stay.ready.ttime;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScientificTest {
+
+    ScientificCalculator scientificCalculator;
+
+    @before
+
+    public void setup(){
+        scientificCalculator = new ScientificCalculator();
+    }
+    @Test
+    public void sineTest(){
+        ScientificCalculator scientificCalculator = ScienticCalculator ""
+    }
+}
